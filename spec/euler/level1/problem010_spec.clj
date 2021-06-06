47bd51d2556248bc1c3e02170cf75a6d8fec8def
gosupark27 <hpark41@wgu.edu>
2021-06-05 22:57:05 -0700

    solve euler 10

--- spec/euler/level1/problem010_spec.clj
+++ spec/euler/level1/problem010_spec.clj
@@ -1,14 +1,47 @@
 (ns euler.level1.problem010-spec
-  (:require
-    [speclj.core :refer :all]
-    [euler.level1.problem010 :refer :all]))
+ (:require
+  [speclj.core :refer :all]
+  [euler.level1.problem010 :refer :all]))
 
 ; https://projecteuler.net/problem=10
 
 (describe "Euler Problem #10"
 
-  (it "Solves #10"
-    (should= -1 (euler-10 -1)))
+ (it "check if n is not divisible"
+  (should (non-divisible? 3 2))
+  (should-not (non-divisible? 4 2))
+  (should (non-divisible? 4 3))
   )
+
+ (it "check if n prime"
+  (should (prime? 2))
+  (should (prime? 3))
+  (should-not (prime? 4))
+  (should (prime? 5))
+  (should-not (prime? 45))
+  (should-not (prime? 10001))
+  )
+
+ (it "get primes below n"
+  (should= [] (get-primes-below-n 2))
+  (should= [2] (get-primes-below-n 3))
+  (should= [2 3] (get-primes-below-n 4))
+  (should= [2 3 5 7] (get-primes-below-n 10))
+  )
+
+ (it "get sum of primes below n"
+  (should= 0 (get-sum-primes-below-n 2))
+  (should= 2 (get-sum-primes-below-n 3))
+  (should= 5 (get-sum-primes-below-n 4))
+  (should= 17 (get-sum-primes-below-n 10))
+  )
+
+ (it "Solves #10"
+  (should= 17 (euler-10 10))
+  (should= 28 (euler-10 12))
+  (should= 41 (euler-10 14))
+  (should= 142913828922 (euler-10 2e6))
+  )
+ )
 
 (run-specs)
--- src/euler/level1/problem010.clj
+++ src/euler/level1/problem010.clj
@@ -1,4 +1,18 @@
 (ns euler.level1.problem010)
 
+(defn non-divisible? [n div]
+  (not (= 0 (mod n div))))
+
+(defn prime? [n]
+  (= 0 (count (filter #(false? (non-divisible? n %)) (range 2 (inc (int (Math/sqrt n))))))))
+
+(defn get-primes-below-n [n]
+  (filter #(prime? %) (range 2 n)))
+
+(defn get-sum-primes-below-n [n]
+  (apply + (get-primes-below-n n)))
+
 (defn euler-10 [n]
-  )
+  (get-sum-primes-below-n n))
+
+
